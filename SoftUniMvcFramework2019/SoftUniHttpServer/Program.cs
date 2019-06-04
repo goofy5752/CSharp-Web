7e0672719666677a2672cb015bdbe41082c0edc8
malloy5752 <m.asenov5752@gmail.com>
2019-06-04 11:26:13 +0300

    Code Refactoring

--- SoftUniMvcFramework2019/SoftUniHttpServer/Program.cs
+++ SoftUniMvcFramework2019/SoftUniHttpServer/Program.cs
@@ -10,7 +10,7 @@ namespace SoftUniHttpServer
         static void Main(string[] args)
         {
             const string NewLine = "\r\n";
-            TcpListener tcpListener = new TcpListener(
+            var tcpListener = new TcpListener(
                 IPAddress.Loopback, 80);
 
             tcpListener.Start();
@@ -18,24 +18,24 @@ namespace SoftUniHttpServer
             while (true)
             {
                 // TCP / UDP
-                TcpClient client = tcpListener.AcceptTcpClient();
-                using (NetworkStream stream = client.GetStream())
+                var client = tcpListener.AcceptTcpClient();
+                using (var stream = client.GetStream())
                 {
-                    byte[] requestBytes = new byte[100000];
+                    var requestBytes = new byte[100000];
                     // Thread.Sleep(10000);
-                    int readBytes = stream.Read(requestBytes, 0, requestBytes.Length);
+                    var readBytes = stream.Read(requestBytes, 0, requestBytes.Length);
                     var stringRequest = Encoding.UTF8.GetString(requestBytes, 0, readBytes);
                     Console.WriteLine(new string('=', 70));
                     Console.WriteLine(stringRequest);
-                    string responseBody = "<form method='post'><input type='text' name='tweet' placeholder='Enter tweet...' /><input name='name' /><input type='submit' /></form>";
-                    string response = "HTTP/1.0 200 OK" + NewLine +
+                    var responseBody = "<form method='post'><input type='text' name='tweet' placeholder='Enter tweet...' /><input name='name' /><input type='submit' /></form>";
+                    var response = "HTTP/1.0 200 OK" + NewLine +
                                       "Content-Type: text/html" + NewLine +
                                       // "Location: https://google.com" + NewLine +
                                       // "Content-Disposition: attachment; filename=index.html" + NewLine +
                                       "Server: MyCustomServer/1.0" + NewLine +
                                       $"Content-Length: {responseBody.Length}" + NewLine + NewLine +
                                       responseBody;
-                    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                    var responseBytes = Encoding.UTF8.GetBytes(response);
                     stream.Write(responseBytes, 0, responseBytes.Length);
                 }
             }
